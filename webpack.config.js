d819cc5639b20127b1ca484bf1699ce5857a76fa
krava9999 <kosmo@internet.ru>
2021-11-29 17:30:40 +1000

    этап 2 установка правил

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,37 @@
-﻿
+﻿const {
+    resolve
+} = require('path'); // работаем с путями
+
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // работаем с плагином 
+
+module.exports = {
+    entry: './src/main.js', // точка входя для будущего js 
+    output: {
+        path: resolve(__dirname, 'build'), //  cобираем проект в файл build
+        filename: 'main.[contenthash].js' //  имя файла с хэшэм
+    },
+
+    module: {
+        // задаем правила для преобразования файлов
+        rules: [{
+                test: /\\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\\.scss$/i, // преобразование css
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, 'index.html')
+        })
+    ]
+
+
+};
